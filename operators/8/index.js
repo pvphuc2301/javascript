cbafaad24525f2365b184de6fbbc19d2889a5d49
Phan Phuc <pvphuc2301@gmail.com>
2020-04-23 14:23:33 +0700

    practice

---
+++ operators/8/index.js
@@ -0,0 +1,5 @@
+var a = [1, 2];
+var b = [1, 2];
+console.log(a === b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù giá trị của 2 array có vẻ giống nhau. Viết câu trả lời ở dưới đây.
+//do biến a và b lưu giá trị tham chiếu đến 2 array khác nhau
