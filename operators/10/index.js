d22bc835f4aac30e53a60a515efd74f6525e04ec
Phan Phuc <pvphuc2301@gmail.com>
2020-04-23 14:43:43 +0700

    practice

---
+++ operators/10/index.js
@@ -0,0 +1,6 @@
+var a = '1000';
+var b = '200';
+
+console.log(a > b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù 1000 trông có vẻ lớn hơn 200. Viết câu trả lời ở dưới đây.
+// khi so sánh 2 biến kiểu string chương trình sẽ so sánh ký tự đầu tiên của biến a và ký tự đầu tiên của biến b, do '1' nhở hơn '2' nên kết quả là false
