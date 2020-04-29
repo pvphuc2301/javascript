afdae3dfab0fd9dfd614cd5d491a0c06a8659738
Phan Phuc <pvphuc2301@gmail.com>
2020-04-29 15:45:47 +0700

    practice

---
+++ sync-async/1/index.js
@@ -0,0 +1,16 @@
+/**
+ * Sử dụng module `request` (https://www.npmjs.com/package/request) để tải dữ liệu từ đường link sau về và hiển thị ra màn hình:
+ * https://randomuser.me/api/
+ * 
+ * Sử dụng console.time và console.timeEnd để hiển thị ra thời gian tải link. Tìm hiểu về 2 hàm này tại: https://alligator.io/js/console-time-timeend/
+ * hoặc google 'console.time'
+ */
+const request = require('request');
+
+console.time('time request');
+request('https://randomuser.me/api/', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+console.timeEnd('time request');
