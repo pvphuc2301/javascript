820a1e1f17202b0ec4ab391e3ced872e83f0737e
Phan Phuc <pvphuc2301@gmail.com>
2020-05-02 21:06:52 +0700

    practice

---
+++ setTimeout/1/index.js
@@ -0,0 +1,14 @@
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms
+ */
+function doAfter(fn, time) {
+    setTimeout(fn, time);
+}
+
+doAfter(function() {
+    console.log('hello');
+}, 3000);
+
